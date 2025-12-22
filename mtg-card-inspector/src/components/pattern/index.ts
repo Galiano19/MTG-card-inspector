dbd1af75e04b02d7e4b8f198a2ecd5a65abffa19
Carlos Galiano <galihorizon21@gmail.com>
2025-12-22 17:57:51 +0100

    fix uneeded import

--- mtg-card-inspector/src/components/pattern/index.ts
+++ mtg-card-inspector/src/components/pattern/index.ts
@@ -1,7 +1,6 @@
 export { default as MTGCardSearch } from "./MTGCardSearch/MTGCardSearch";
 export { default as SearchBar } from "./SearchBar/SearchBar";
 export { default as CardDisplay } from "./CardDisplay/CardDisplay";
-export { default as WelcomeState } from "./WelcomeState/WelcomeState";
 export { default as PriceComparison } from "./PriceComparison/PriceComparison";
 export { default as LoadingState } from "./LoadingState/LoadingState";
 export { default as ErrorState } from "./ErrorState/ErrorState";
