151a2d2015fac16a5c2ffcb6ede42fee7a3d732e
Carlos Galiano <galihorizon21@gmail.com>
2026-02-06 14:52:30 +0100

    extract meta info component

--- mtg-card-inspector/src/components/pattern/Card/Info.tsx
+++ mtg-card-inspector/src/components/pattern/Card/Info.tsx
@@ -1,8 +1,6 @@
 import { CardFace, ScryfallCard } from "@/types/scryfall";
 import Name from "./Name";
-import { ManaSymbol } from "./ManaSymbol";
-import { getRarityColor } from "@/lib/card/utils";
-import SetInfo from "./SetInfo";
+import MetaInfo from "./MetaInfo";
 
 interface InfoProps {
   nameLabel?: string;
@@ -14,32 +12,7 @@ export default function Info({ nameLabel, face, card }: InfoProps) {
   return (
     <div className="flex flex-col gap-4">
       <Name nameLabel={nameLabel || undefined} card={card} activeFace={face} />
-      <div className="flex flex-col md:flex-row gap-2 md:gap-4 p-4 rounded-xl border  bg-[--clr-surface-a20] backdrop-blur shadow-xl shadow-[--clr-surface-a0]/30">
-        <div className="flex flex-col md:border-r border-b md:border-b-0 md:pr-8 pb-4 md:pb-0">
-          <span className=" text-xs font-bold uppercase tracking-wider">
-            Mana cost
-          </span>
-          <span className="mt-2">
-            <ManaSymbol card={card} face={face} />
-          </span>
-        </div>
-        <div className="flex flex-col md:border-r border-b md:border-b-0 md:pr-8 pb-4 md:pb-0">
-          <span className=" text-xs font-bold uppercase tracking-wider">
-            Rarity
-          </span>
-          <span className={`text-sm font-bold mt-2 uppercase `}>
-            {card.rarity}
-          </span>
-        </div>
-        <div className="flex flex-col">
-          <span className=" text-xs font-bold uppercase tracking-wider">
-            Set
-          </span>
-          <span className="text-sm font-bold mt-1 flex items-center gap-2">
-            <SetInfo {...card} />
-          </span>
-        </div>
-      </div>
+      <MetaInfo card={card} face={face} />
     </div>
   );
 }
