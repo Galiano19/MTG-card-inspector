4892939276ea72ac6d61d8ddf9d269de14094683
Carlos Galiano <galihorizon21@gmail.com>
2026-02-06 18:20:34 +0100

    add temporary loading state for card page

--- mtg-card-inspector/src/app/card/page.tsx
+++ mtg-card-inspector/src/app/card/page.tsx
@@ -4,6 +4,7 @@ import useCardRoute from "@/hooks/useCardRoute";
 import LayoutPage from "@/components/pattern/LayoutPage/LayoutPage";
 import LayoutItem from "@/components/ui/layoutItem";
 import Card from "@/components/pattern/Card/Card";
+import { ErrorState } from "@/components/pattern";
 
 export default function CardPage() {
   const { handleSearch, handleRetry, card, isLoading, error } = useCardRoute({
@@ -13,6 +14,34 @@ export default function CardPage() {
   const showLoading = isLoading;
   const showError = !!error && !showLoading;
   const showCard = !!card && !showLoading && !error;
+
+  if (showLoading) {
+    return (
+      <LayoutPage>
+        <LayoutItem>
+          <div className="flex items-center justify-center h-[70vh]">
+            <div className="text-sm text-[--clr-surface-tonal-a20]">
+              Loading...
+            </div>
+          </div>
+        </LayoutItem>
+      </LayoutPage>
+    );
+  }
+
+  if (showError) {
+    return (
+      <LayoutPage>
+        <LayoutItem>
+          <div className="flex items-center justify-center h-[70vh]">
+            <div className="text-sm text-[--clr-surface-tonal-a20]">
+              <ErrorState error={"Card not found"} />
+            </div>
+          </div>
+        </LayoutItem>
+      </LayoutPage>
+    );
+  }
 
   return (
     <LayoutPage>
